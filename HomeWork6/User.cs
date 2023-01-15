33d714080b854684cd6a929ddf4a7f3926869bb5
Farnoush Shahin <farnoush.shahin@gmail.com>
2023-01-15 19:34:03 +0330

    c

---
+++ HomeWork6/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork6
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int mobileNumber { get; set; }
+        public DateTime birthDate  { get; set; }
+        public DateTime creationDate { get; set; }
+    }
+}
---
+++ HomeWork6/UserMenu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork6
+{
+    public enum UserMenu
+    {
+        DefineUser=1,
+        ShowListOfUsers,
+        UpdateUser,
+        DeleteUser,
+        LogOut
+    }
+}
